5c9f1893f2b6c5683609ed1c63e7a62eb61a20d6
Henry Lee <henryleeworld@gmail.com>
2023-04-02 00:19:56 +0800

    feat:初始化

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Flynsarmy\CsvSeeder\CsvSeeder;
+use Illuminate\Support\Facades\DB;
+
+class UserSeeder extends CsvSeeder
+{
+	public function __construct()
+	{
+		$this->table = 'users';
+		$this->filename = base_path().'/database/seeders/csvs/users.csv';
+	}
+
+	public function run()
+	{
+		// Recommended when importing larger CSVs
+		DB::disableQueryLog();
+
+		// Uncomment the below to wipe the table clean before populating
+		DB::table($this->table)->truncate();
+
+		parent::run();
+	}
+}
